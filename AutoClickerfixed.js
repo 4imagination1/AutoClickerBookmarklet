e34e17d2174bb01b1faf21a5302975b060958672
4imagination1 <97059860+4imagination1@users.noreply.github.com>
2023-02-24 12:16:12 -0500

    auto clcker fixed
    bhjhvghftcgvhvhb

--- AutoClickerfixed.js
+++ AutoClickerfixed.js
@@ -1,6 +1,6 @@
 javascript:
 
-var DELAY = 1;
+var DELAY = 0;
 var autoClickerStyleElement = document.createElement("style");
 autoClickerStyleElement.innerHTML="*{cursor: crosshair !important;}";
 document.body.appendChild(autoClickerStyleElement);
@@ -10,21 +10,21 @@ function addClicker(e) {
 	}
 	if(e.target.classList.contains("auto-clicker-target")) {
 		e.target.classList.remove("auto-clicker-target");
-	} else {
+	} else { target= click[acsseses if true]click [10000]cps
 		e.target.classList.add("auto-clicker-target");
 	}
 	document.body.removeChild(autoClickerStyleElement);
 	document.body.removeEventListener("click", addClicker);
 	e.preventDefault();
 	
-	autoClick(e.target);
+	autoClick(e.target)=screen text or demo of true;
 
 }
-function autoClick(element) {
+function autoClick(element) {=99tosix acsses 
 	if(element.classList.contains("auto-clicker-target")) {
 		element.click();
 		setTimeout(function(){ autoClick(element); }, DELAY);
 	}
 }
-document.body.addEventListener("click", addClicker, 0);
-
+document.body.addEventListener("click", addClicker, 1000000);
+gy+ifTrue[]=true
